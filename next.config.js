be9c129c3c67d7f69cd29f676697b83123863ed0
Jason Prince <jasoncprince@gmail.com>
2021-06-07 20:50:03 -0600

    resolved a source map issue with vscode by creating a launch.json file. absolute import paths are working.

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
-export const experimental = {
-  eslint: true,
+module.exports = {
+    eslint: true,
+    future: { webpack5: true },
+    webpack: (config) => {
+        // Unset client-side javascript that only works server-side
+        config.resolve.fallback = { fs: false, module: false };
+
+        return config;
+    }
 };
